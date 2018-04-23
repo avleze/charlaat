2eba5ab11f89214edaff41246ddbd80b93af6b8b
David Borrego <dborrego@atsistemas.com>
2018-04-23 14:14:10 +0200

    proyecto

---
+++ src/main/java/com/dbg/jenkinspipelinebase/JenkinsPipelineBaseApplication.java
@@ -0,0 +1,12 @@
+package com.dbg.jenkinspipelinebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineBaseApplication.class, args);
+	}
+}
